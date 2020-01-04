fda01ab0ec3f7c446583053d8329ff748d1b4ed1
kanishkjain1995 <kanishk5991@gmail.com>
2020-01-04 20:56:50 +0530

    reparamterization trick

--- vanilla_vae/models.py
+++ vanilla_vae/models.py
@@ -3,6 +3,38 @@ import torch.nn as nn
 
 class VAE(nn.Module):
     
-    def __init__(self, input_size, latent_size, output_size):
+    def __init__(self, in_features, latent_size, batch_size):
+        super(VAE, self).__init__()
+        self.enc_fc_1 = nn.Linear(in_features, 512)
+        self.enc_fc_2 = nn.Linear(512, 256)
+
+        self.latent_size = latent_size
+
+        self.mu = nn.Linear(256, latent_size)
+        self.log_sigma = nn.Linear(256, latent_size)
         
-        pass
+        self.epsilon = torch.distributions.Normal(torch.tensor([0.0]), torch.tensor([1.0]))
+
+        # self.conv_1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=3, stride=2)
+        # self.conv_2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=2)
+    
+    def forward(self, input_):
+        input_ = input_.reshape(-1, 784)
+        print("{} 0th level output shape".format(input_.shape))
+        out = self.enc_fc_1(input_)
+        print("{} 1st level output shape".format(out.shape))
+        out = self.enc_fc_2(out)
+        print("{} 2nd level output shape".format(out.shape))
+
+        mu = self.mu(out)
+        log_sigma = self.log_sigma(out)
+
+        eps = self.epsilon.sample(self.latent_size)
+        print(eps.shape)
+        return mu, log_sigma
+        # print("{} 0th level output shape".format(input_.shape))
+        # out = self.conv_1(input_)
+        # print("{} 1st level output shape".format(out.shape))
+        # out = self.conv_2(out)
+        # print("{} 2nd level output shape".format(out.shape))
+        # return torch.flatten(out)
--- vanilla_vae/train.py
+++ vanilla_vae/train.py
@@ -3,15 +3,19 @@ import torchvision
 import torchvision.transforms as transforms
 from models import VAE
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("{} device is being used!!".format(device))
 
 # vae params
-latent_size = 1000
+latent_size = 100
 learning_rate = 1e-3
 
 epochs = 100
-batch_size = 64
+batch_size = 2
+in_channels = 784
 
 train_dataset = torchvision.datasets.MNIST(root="./data", train=True, transform=transforms.ToTensor(), download=True)
 test_dataset = torchvision.datasets.MNIST(root="./data", train=False, transform=transforms.ToTensor())
@@ -19,6 +23,27 @@ test_dataset = torchvision.datasets.MNIST(root="./data", train=False, transform=
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
+# for ind, (image, label) in enumerate(train_loader):
+#     print(image.shape, label.shape, label)
+
+#     img = image[0].permute(1, 2, 0)
+#     print(img.shape)
+
+#     np_img = img.numpy().squeeze()
+#     plt.imshow(np_img)
+#     plt.show()
+#     break
 ### 
 
-model = VAE()
+train_iter = iter(train_loader)
+image, label = next(train_iter)
+
+model = VAE(in_channels, latent_size)
+
+output = model(image)
+print(output)
+
+
+
+# print(model.parameters)
+# optimizer = torch.optim.Adam(lr=learning_rate, model.parameters())
