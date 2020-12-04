baba4bc751900c4cf6ed191ac63ad4a341c31c26
kanishk <kanishk5991@gmail.com>
2020-12-04 23:56:35 +0530

    try generalizability experiments

--- vanilla_vae/train.py
+++ vanilla_vae/train.py
@@ -19,16 +19,9 @@ def show(img):
     plt.imshow(np.transpose(npimg, (1,2,0)), interpolation='nearest')
     plt.show()
 
-# Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar, beta=5):
     BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-
     return BCE + beta*KLD
 
 # vae params
