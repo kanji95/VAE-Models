2fca655e770057cc4ae232039d3046137e16c28c
kanji95 <kanishk5991@gmail.com>
2020-12-05 23:47:07 +0530

    try conv vae

--- vanilla_vae/models.py
+++ vanilla_vae/models.py
@@ -7,11 +7,13 @@ class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
 
-        self.fc1 = nn.Linear(784, 400)
-        self.fc21 = nn.Linear(400, 32)
-        self.fc22 = nn.Linear(400, 32)
-        self.fc3 = nn.Linear(32, 400)
-        self.fc4 = nn.Linear(400, 784)
+        self.fc1 = nn.Linear(28*28, 400)
+        self.fc21 = nn.Linear(400, 128)
+        self.fc22 = nn.Linear(400, 128)
+        self.fc3 = nn.Linear(128, 512)
+        self.fc4 = nn.Linear(512, 28*28)
+
+        #self.classifier = nn.Sequential(nn.Linear(64, 128), nn.ReLU(), nn.Linear(128, 10), nn.Softmax(dim=-1))
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5*logvar)
@@ -27,6 +29,7 @@ class VAE(nn.Module):
         return torch.sigmoid(self.fc4(out))
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
+        mu, logvar = self.encode(x.view(-1, 28*28))
         z = self.reparameterize(mu, logvar)
+        #comb = torch.cat([mu, logvar], dim=-1)
         return self.decode(z), mu, logvar
--- vanilla_vae/train.py
+++ vanilla_vae/train.py
@@ -19,19 +19,19 @@ def show(img):
     plt.imshow(np.transpose(npimg, (1,2,0)), interpolation='nearest')
     plt.show()
 
-def loss_function(recon_x, x, mu, logvar, beta=5):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+def vae_loss(recon_x, x, mu, logvar, beta=5):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 28*28), reduction='sum')
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     return BCE + beta*KLD
 
 # vae params
 learning_rate = 1e-3
 
-epochs = 10
+epochs = 30
 batch_size = 128
 
-train_dataset = torchvision.datasets.MNIST(root="./data", train=True, transform=transforms.ToTensor(), download=True)
-test_dataset = torchvision.datasets.MNIST(root="./data", train=False, transform=transforms.ToTensor())
+train_dataset = torchvision.datasets.MNIST(root="/scratch/anurag_deshmukh/data", train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = torchvision.datasets.MNIST(root="/scratch/anurag_deshmukh/data", train=False, transform=transforms.ToTensor())
 
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
@@ -39,37 +39,99 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch
 test_iter = iter(test_loader)
 
 model = VAE()
+classifier = nn.Sequential(nn.Linear(256, 512), nn.ReLU(), nn.Dropout(0.2), nn.Linear(512, 256), nn.ReLU(), nn.Dropout(0.2), nn.Linear(256, 10), nn.Softmax(dim=-1))
 
-optim = torch.optim.Adam(model.parameters(), lr=learning_rate)
+#params = list(model.parameters()) + list(classifier.parameters())
+model_optim = torch.optim.Adam(model.parameters(), lr=learning_rate)
+class_optim = torch.optim.Adam(classifier.parameters(), lr=learning_rate)
 
 now = datetime.now()
 writer = SummaryWriter('./plots/vae_{}'.format(now.strftime("%d_%H_%M")))
 
 def train(epoch):
     model.train()
+    classifier.train()
+
     train_loss = 0
+    v_loss = 0
+    c_loss = 0
+
+    train_acc = 0
+
     niter = 0
-    for index, (imgs, _) in enumerate(train_loader):
-        optim.zero_grad()
-        recon_imgs, mu, logvar = model(imgs)
-        loss = loss_function(recon_imgs, imgs, mu, logvar)
-        train_loss += loss.item()
-        loss.backward()
-        optim.step()
+    
+    criterion = nn.CrossEntropyLoss(reduction='sum')
+
+    data_len = len(train_loader.dataset)
+    loader_len = len(train_loader)
+
+    #import pdb; pdb.set_trace();
+
+    for index, (imgs, labels) in enumerate(train_loader):
         
-        writer.add_scalar('Train loss', loss.item()/len(imgs), niter + epoch*len(train_loader.dataset))
-        niter += len(imgs)
+        batch_len = len(imgs)
+
+        model.zero_grad()
+        recon_imgs, mu, logvar = model(imgs)
+        v_loss = vae_loss(recon_imgs, imgs, mu, logvar)
+        v_loss.backward()
+        model_optim.step()
+
+        class_optim.zero_grad()
+        with torch.no_grad():
+            comb = torch.cat([mu, logvar], dim=-1)
+        probs = classifier(comb)
+        c_loss = criterion(probs, labels)
+        c_loss.backward()
+        class_optim.step()
+
+        for name, param in classifier.named_parameters():
+            if 'weight' in name:
+                param.data = torch.tanh(param.data)
+
+        total_loss = v_loss + c_loss
+        train_loss += total_loss
+       
+        preds = probs.argmax(dim=-1)
+        train_acc += sum(preds == labels).item()
+        
+        writer.add_scalar('Train loss', total_loss/batch_len, niter + epoch*data_len)
+        writer.add_scalar('VAE loss', v_loss/batch_len, niter + epoch*data_len)
+        writer.add_scalar('Classification loss', c_loss/batch_len, niter + epoch*data_len)
+
+        niter += batch_len
+
+        writer.add_scalar('Train Accuracy', train_acc/niter, niter + epoch*data_len)
+        
 
         if index%50 == 0:
-            print("Epoch: {} [{:6}/{} ({:3.2f}%)]\t Avg. Loss: {:.3f}".format(epoch, index*len(imgs), len(train_loader.dataset), 100. * index/len(train_loader), loss.item()/len(imgs)))
+            print("Epoch: {} [{:6}/{}]\t Avg. Loss: {:.3f} VAE Loss: {:.3f} Class Loss: {:.3f} Avg. Acc: {:.3f}".format(epoch, index*batch_len, data_len, total_loss/niter, v_loss/batch_len, c_loss, train_acc/niter))
 
 def test(epoch):
     model.eval()
+    classifier.eval()
+
     test_loss = 0
+    v_loss = 0
+    c_loss = 0
+
+    test_acc = 0
+
+    criterion = nn.CrossEntropyLoss()
+
     with torch.no_grad():
-        for index, (imgs, _) in enumerate(test_loader):
+        for index, (imgs, labels) in enumerate(test_loader):
             recon_imgs, mu, logvar = model(imgs)
-            test_loss += loss_function(recon_imgs, imgs, mu, logvar)
+            comb = torch.cat([mu, logvar], dim=-1)
+            probs = classifier(comb)
+
+            v_loss += vae_loss(recon_imgs, imgs, mu, logvar)
+            c_loss += criterion(probs, labels)
+            total_loss = v_loss + c_loss
+            test_loss += total_loss
+     
+            preds = probs.argmax(dim=-1)
+            test_acc += sum(preds == labels).item()
 
             if index == 0:
                 n = min(len(imgs), 16)
@@ -77,8 +139,11 @@ def test(epoch):
                 writer.add_image('Recon Images', make_grid(comparison), epoch)
                 # show(make_grid(comparison, padding=10))
 
-    test_loss /= len(test_loader.dataset)
-    print("===============================>\t Avg. Test loss: {:.4f}".format(test_loss))
+    test_loss /= len(test_loader)
+    v_loss /= len(test_loader)
+    c_loss /= len(test_loader)
+    test_acc /= len(test_loader.dataset)
+    print("===============================>\t Avg. Test loss: {:.4f} VAE Loss: {:.4f} Class Loss: {:.4f} Test Acc: {:.4f}".format(test_loss, v_loss, c_loss, test_acc))
 
 for epoch in range(epochs):
     train(epoch)
